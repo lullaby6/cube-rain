264160fa5ad1eaf34accf25c2c4248b2dfc0e8a8
LucianoBrumer <lucianobrumer5@gmail.com>
2024-10-09 08:12:50 -0300

    xd

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ document.addEventListener("orientationchange", event => {
             /* Device is in portrait mode */
     }
 });
+
+function checkOrientation() {
+    if (window.innerHeight > window.innerWidth) {
+        document.body.style.transform = "none";
+    } else {
+        document.body.style.transform = "rotate(90deg) translate(0, -100vw)";
+    }
+}
+
+window.addEventListener("resize", checkOrientation);
+window.addEventListener("orientationchange", checkOrientation);
+
+checkOrientation();
